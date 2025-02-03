db0b1f143e472f35f7d70d85426c3753a8e054bd
Shateek Jones <shateekj4@nycstudents.net>
2025-02-03 16:29:41 +0000

    First day

--- LAB1/practice/script.js
+++ LAB1/practice/script.js
@@ -1,12 +1,41 @@
 // List 4 data types in a multiline comment
 
+/*
+Boolean
+Int
+Float
+String
+*/
+
 // Typeof
+console.log(typeof 42)
+console.log(typeof "blue")
+console.log(typeof false)
+
+var x = 1;
+console.log(typeof x)
 
 // Let vs Const vs Var
+var x = 2;
+let age = 15;
+age = 26;
 
 // Reassignment (no let)
-
+age = 20;
+age = 30;
+age = 40;
 // Essential Operations
+let sum = age + 2;
+console.log(sum)
+sum = sum - 5;
+console.log(sum)
+sum = sum / 2
+console.log(sum)
+sum = sum + 10;
+sum += 10;
+console.log(sum)
+sum -= 7;
+console.log(sum)
 
 // Additional operations
 
